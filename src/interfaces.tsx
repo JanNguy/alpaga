bfa8e9b4e468e4112e7e099562ee2c6e3423df3e
Jan Nguyen <jan.nguyen@epitech.eu>
2025-10-11 03:07:27 +0200

    add: interfaces

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -4,3 +4,13 @@ export interface UserText {
     message: string;
     type: "user" | "assistant";
 }
+
+export interface Unit {
+    name: string;
+    usage: string;
+    parameters: string;
+    weight: number;
+    nb_dl: string;
+    t_update: string; //number mais transformer en heure/jour/mois
+    likes: number;
+}
